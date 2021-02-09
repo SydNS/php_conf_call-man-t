542e5164146c4d55d96fda260701f95a9db0c094
root <root@localhost.localdomain>
2021-02-09 11:06:00 -0500

    added images

--- index.php
+++ index.php
@@ -11,31 +11,34 @@
     </head>
     <body>
 
-        <div class="card">
-          <div class="card-body">
+          <div class="card">
+            <div class="card-body">
           <div class="container-fluid" id="loginform">
             <h2 id="logintitle">
                 Login
             </h2>
-            <form action="auth.php" class="was-validated" method="POST">
+            <form action="auth.php"  class="was-validated" method="POST">
                 <div class="form-group">
                   <label for="uname">Username:</label>
-                  <input type="text" class="form-control" id="uname" placeholder="Enter username" name="uname" required>
+                  <input type="text" class="form-control" id="uname" placeholder="Enter Username" name="uname" required>
                   
                 </div>
+
+      
+
                 <div class="form-group">
                   <label for="pwd">Password:</label>
-                  <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" required>
+                  <input type="password" class="form-control" id="pwd" placeholder="Enter Password" name="pswd" required>
                   
                 </div>
+
                 
-                <button type="submit" class="btn btn-primary">Login</button>
+                <button type="submit" class="btn btn-primary"  id="btn1">Login</button>
                 <br>
                 <br>
 
-                <a href="signup.php" class="info" id="signuplink">No Account? Create account</a>
+                <a href="signup.php" class="info" id="signuplink">Create account ? </a>
               </form>
-            
         </div>
           </div>
         </div>
