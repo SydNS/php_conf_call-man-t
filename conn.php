4e8cc1bc6b5317176b5d1ad2bb763676aba6a006
root <root@localhost.localdomain>
2021-02-10 16:58:46 -0500

    modified the connection code

--- conn.php
+++ conn.php
@@ -1,4 +1,14 @@
-<?php 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "conference_db";
 
-$conn = mysqli_connect('localhost','root','','conference_db');
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 ?>
