09f00b93705193eb2e47bdb4dae156d4d4160e62
root <root@localhost.localdomain>
2021-02-10 15:43:10 -0500

    using the include function to dissemeinate the files

--- func.php
+++ func.php
@@ -1,27 +1,4 @@
 <?php 
+ include_once('auth.php');
 
-$conn = mysqli_connect('localhost','root','','conference_db');
-
-if (isset($_POST['login_submit'])) {
-
-    $uname=$_POST['uname'];
-    $paswd=$_POST['pswd'];
-
-
-$query="select * from users_login_tb where username='$uname' and password='$paswd'";
-
-$result=mysqli_query($conn,$query);
-
-if (mysqli_num_rows($result)==1) {
-   
-    header("Location:adminpanel.php");
-    echo '<script>alert("Enter Correct Details")</script>';
-
-}else {
-    echo "window.open('index.php')";
-    header("Location:index.php");
-    echo '<script>alert("Enter Correct Details")</script>';
-        
-}
-}
 ?>
