963454484d4518495ce12c99ed94697d7b273aca
root <root@localhost.localdomain>
2021-02-10 15:39:18 -0500

    created an admin page for being a destination

---
+++ adminpanel.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Admin Panel</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bootstrap.min.css">
+        <link rel="stylesheet" href="auth.css">
+    </head>
+    <body style="background-image: url('images/conf5.png')">
+
+          <div class="card">
+
+
+            <div class="card-body">
+          <div class="container-fluid" id="loginform">
+            <h2 id="logintitle">
+                Admin Panel
+            </h2>
+
+        </div>
+          </div>
+        </div>
+
+        
+        <script src="bootstrap.min.js" async defer></script>
+        <script src="jquery.min.js" async defer></script>
+    </body>
+</html>
