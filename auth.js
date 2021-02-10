4c655c78282777045eed3f7934d0c1fa059f145f
root <root@localhost.localdomain>
2021-02-10 16:54:20 -0500

    validating form inputs

--- auth.js
+++ auth.js
@@ -1,12 +1,23 @@
 
 function passwordMatch() {
-   var pswd=document.getElementsByName("paswd")
-var pswd2=document.getElementsByName("paswd2")
-var signup_submit=document.getElementsByName("signup_submit")
+    var pswd=document.getElementsByName("paswd")
+    var pswd2=document.getElementsByName("paswd2")
 
-var x1=pswd.value
-var x2=pswd2.value
+    var x1=pswd.value
+    var x2=pswd2.value
+
     if (x1!=x2) {
-    return alert("Passwords dont Match")    
+        alert("Passwords dont Match")    
+    }
 }
-}
+
+
+function tel() {
+    var tel=document.getElementsByName("tel")
+    var x1=tel.value
+ 
+    if (x1.isNan()) {
+             alert("Tel number")    
+    }
+ }
+ 
