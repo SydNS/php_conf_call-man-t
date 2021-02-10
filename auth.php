dab01d60a1468395dffe15433e1a6e4ad12f489b
root <root@localhost.localdomain>
2021-02-10 16:38:59 -0500

    checking if passwords are filled in ryt on the backend

--- auth.php
+++ auth.php
@@ -27,9 +27,17 @@ if (mysqli_num_rows($result)==1) {
 if (isset($_POST['signup_submit'])) {
 
     $uname=$_POST['uname'];
+    $tel=$_POST['tel'];
+    $residence=$_POST['residence'];
     $paswd=$_POST['pswd'];
+    $paswd2=$_POST['pswd2'];
 
-
+    if ($paswd!=$paswd2) {
+        echo '<script>alert("Password dont Match")</script>';
+        echo "window.open('index.php')";
+          
+    }else {
+        
 $query="select * from users_login_tb where username='$uname' and password='$paswd'";
 
 $result=mysqli_query($conn,$query);
@@ -37,7 +45,6 @@ $result=mysqli_query($conn,$query);
 if (mysqli_num_rows($result)==1) {
    
     header("Location:adminpanel.php");
-    echo '<script>alert("Enter Correct Details")</script>';
 
 }else {
     echo "window.open('index.php')";
@@ -45,6 +52,10 @@ if (mysqli_num_rows($result)==1) {
     echo '<script>alert("Enter Correct Details")</script>';
         
 }
+        
+    }
+
+
 }
 
 ?>
