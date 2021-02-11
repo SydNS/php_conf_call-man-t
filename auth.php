29d297e7840ca42f076d719773cf64a013bf5f50
root <root@localhost.localdomain>
2021-02-10 19:58:22 -0500

    changes

--- auth.php
+++ auth.php
@@ -27,41 +27,48 @@ mysqli_close($conn);
 
 if (isset($_POST['signup_submit'])) {
 
+
+
     $uname=$_POST['uname'];
     $tel=$_POST['tel'];
     $residence=$_POST['residence'];
     $paswd=$_POST['pswd'];
     $paswd2=$_POST['pswd2'];
 
+    $sql = 'insert into user_details ( username, password,telephone,class, school, residence) VALUES ("syd nsiimbe", "sydney", 0701234567, "p8","Kps","Klac");';
+    $sqlogin = 'insert into users_login_tb ( username, password) VALUES ("syd nsiimbe", "sydney");';
+        
     if ($paswd!=$paswd2) {
         
         echo '<script>alert("Passwords dont Match")</script>';
         
         header("Location:signup.php");
             
-          
     }else {
         
-
-        $sql = 'insert into user_details ( username, password,telephone,class, school, residence) VALUES ("syd nsiimbe", "sydney", 0701234567, "p8","Kps","Klac");';
-        $sqlogin = 'insert into users_login_tb ( username, password) VALUES ("syd nsiimbe", "sydney");';
-        
-        if (mysqli_query($conn, $sql) && mysqli_query($conn, $sqlogin)) {
+            
             if (mysqli_query($conn, $sql) && mysqli_query($conn, $sqlogin)) {
-            header("Location:index.php");
 
-            echo "<script> alert('New record created successfully')</script>";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    header("Location:index.php");
+
+                    echo "<script> alert('New record created successfully')</script>";
+
+                 } else {
+
+
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+
+        
+        
+                
+     
+
+
+   
         }
-        
-        mysqli_close($conn);
-
-        
-        
-    }
+        mysqli_close($conn); 
 
 
-}
 
 ?>
